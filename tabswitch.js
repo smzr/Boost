a7c801817becb5f1b3a6ec4e3ec154c72fcfc222
Samazer <smzr@pm.me>
2018-07-18 11:12:57 +0100

    styling overhaul

--- tabswitch.js
+++ tabswitch.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   $('.tabs > li > a').click(function(event){
-     var active_tab = $('.tabs > li.active > a').attr('href');
-     $(active_tab).removeClass('active');
-     $(active_tab).addClass('hide');
 
-     var actived_tab = $('.tabs > li.active');
-     actived_tab.removeClass('active underline');
+    event.preventDefault();
 
-     $(this).parents('li').addClass('active underline');
+    var active_tab = $('.tabs > li.active > a').attr('href');
+    $(active_tab).removeClass('active');
+    $(active_tab).addClass('hide');
 
-     var target_tab_selector = $(this).attr('href');
-     $(target_tab_selector).removeClass('hide');
+    var actived_tab = $('.tabs > li.active');
+    actived_tab.removeClass('active underline');
+
+    $(this).parents('li').addClass('active underline');
+
+    var target_tab_selector = $(this).attr('href');
+    $(target_tab_selector).removeClass('hide');
     $(target_tab_selector).addClass('active');
   });
 });
